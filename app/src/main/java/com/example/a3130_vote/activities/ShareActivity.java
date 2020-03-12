d38b2b7c053ab9544aab298e4b6782e63e2efb6c
chengxinhai <cxh@dal.ca>
2020-03-11 22:26:10 -0300

    [CODE] Xinhai Cheng & Yanlin Zhu, finish the US: user can share to friends. Finish the share page.

---
+++ app/src/main/java/com/example/a3130_vote/activities/ShareActivity.java
@@ -0,0 +1,21 @@
+package com.example.a3130_vote.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a3130_vote.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+
+
+
+
+
+}
